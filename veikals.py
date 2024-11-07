8617fd369f3d56939c81d6e665101f09d9477e15
martamaurina <martamaurina3@gmail.com>
2024-11-07 14:01:33 +0200

    Update veikals.py

--- veikals.py
+++ veikals.py
@@ -29,7 +29,7 @@ class ShoppingCart:
 class App:
     def __init__(self, master):
         self.master=master
-        self.master.geometry("345x550")
+        self.master.geometry("345x560")
         self.master.title("Veikals")
         self.master.configure(background="#6c6058")
         self.cart=ShoppingCart()
@@ -37,22 +37,22 @@ class App:
         input_frame = tk.Frame(master)
         input_frame.grid(pady=10, padx=20)
 
-        self.name_label=tk.Label(input_frame, text="Nosaukums")
+        self.name_label=tk.Label(input_frame, text="Nosaukums", font=("Helvetica", 10))
         self.name_label.grid(row=1, column=0, padx=5, pady=5)
         self.name_entry = tk.Entry(input_frame)
         self.name_entry.grid(row=1, column=1, padx=20)
 
-        self.quantity_label=tk.Label(input_frame, text ="Daudzums")
+        self.quantity_label=tk.Label(input_frame, text ="Daudzums", font=("Helvetica", 10))
         self.quantity_label.grid(row=2, column=0, padx=5, pady=5)
         self.quantity_entry = tk.Entry(input_frame)
         self.quantity_entry.grid(row=2, column=1, padx=20)
 
-        self.price_label=tk.Label(input_frame, text="Cena")
+        self.price_label=tk.Label(input_frame, text="Cena", font=("Helvetica", 10))
         self.price_label.grid(row=3, column=0, padx=5, pady=5)
         self.price_entry =tk.Entry(input_frame)
         self.price_entry.grid(row=3, column=1, padx=20)
 
-        self.add_button=tk.Button(master, text="Pievienot grozam", command=self.add_to_cart)
+        self.add_button=tk.Button(master, text="Pievienot grozam", command=self.add_to_cart, font=("Helvetica", 10))
         self.add_button.grid(pady=10, padx=20)
 
         self.cart_listbox=tk.Listbox(master, width=50)
@@ -61,7 +61,7 @@ class App:
         self.total_label = tk.Button(master, text="Kopējā cena: 0.00 Eur", font=("Arial", 12))
         self.total_label.grid(pady=10, padx=20)
 
-        self.clear_button = tk.Button(master, text="Dzēst grozu", command=self.clear_cart)
+        self.clear_button = tk.Button(master, text="Dzēst grozu", command=self.clear_cart, font=("Helvetica", 10))
         self.clear_button.grid(pady=5, padx=20)
 
     
@@ -96,7 +96,7 @@ class App:
         self.cart_listbox.delete(0, tk.END)
         self.update_total_price()
 
-
+#font=("Helvetica", 10)
 
 if __name__=="__main__":
     root=tk.Tk()
